00b4784feac95a07f4f274946e8f3da2575e6c1a
daesy13 <daesyramos@hotmail.com>
2019-04-10 14:01:18 +0000

    adding beatles and wishlist

---
+++ beatles.py
@@ -0,0 +1,11 @@
+# 1. Let's get the band back together.
+# Can you please add "Paul" to the end of
+# the beatles list using the appropriate list method?
+# 2. Okay now let's add all of the items in others list to the beatles list.
+# Can you please use the method on lists that allows you to add all
+# values from one list to another?
+
+beatles = ["John"]
+others = ["George", "Ringo"]
+beatles.append("Paul")
+beatles.extend(others)
--- meeting.py
+++ meeting.py
@@ -1,2 +1,11 @@
 attendees = ["Ken", "Alena", "Treasure"]
-print("There are", len(attendees), "attendees currently")
+# this add an item at the end of the list
+attendees.append("Ashley")
+# this modifies the specified list by extending n# of items
+attendees.extend(["James", "Guil"])
+optional_invitees = ["Ben", "Dave"]
+# this way we create a new list where we concatenate 2 lists into 1
+# without distortioning the original lists
+potential_attendees = attendees + optional_invitees
+
+print("There are", len(potential_attendees), "potential attendees currently")
